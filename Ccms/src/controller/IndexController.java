e2a715ee2f360056190ab22fffb93f7c5bdc938a
LeonSC <sasgsc@gmaill.com>
2016-07-30 19:00:36 +0800

    spring init

---
+++ Ccms/src/controller/IndexController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public String index(HttpServletRequest request)
+	{
+		return "index";
+	}
+	
+	@RequestMapping("/building")
+	public String building()
+	{
+		return "building";
+	}
+	
+}
