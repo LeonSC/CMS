675728eac06c15c305ba663808c7e9f4f7acf44d
LeonSC <sasgsc@gmaill.com>
2016-08-06 16:22:13 +0800

    name change

--- Ccms/src/controller/AdminController.java
+++ Ccms/src/controller/AdminController.java
@@ -2,14 +2,22 @@ package controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import model.Page;
+import model.User;
+import service.WriterService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+	@Autowired
+	private WriterService writerService;
+	
 	@RequestMapping("")
 	public String root()
 	{
@@ -29,7 +37,7 @@ public class AdminController {
 		return "admin/index";
 	}
 	
-	@RequestMapping("/editorlist")
+	@RequestMapping("/writerlist")
 	public String editorList(HttpServletRequest request,
 			@RequestParam(value = "act", required = false) String act,
 			@RequestParam(value = "page", required = false) Integer page)
@@ -46,9 +54,11 @@ public class AdminController {
 		request.setAttribute("act", act);
 		request.setAttribute("page", page);
 		
+		Page<User> writerWaitList=this.writerService.getWriterWaitList(page);
 		
+		request.setAttribute("writerWaitList", writerWaitList);
 		
-		return "admin/editorlist";
+		return "admin/writerlist";
 	}
 	
 }
